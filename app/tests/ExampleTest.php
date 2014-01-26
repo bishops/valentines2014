aed55957c8e9177004eacaacf94b56c13c5994f1
Brian <laxnski@gmail.com>
2014-01-25 23:15:10 -0800

    Adds testing and testing config

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -7,8 +7,42 @@ class ExampleTest extends TestCase {
 	 *
 	 * @return void
 	 */
-	public function testBasicExample()
+	public function setUp()
 	{
+		parent::setUp();
+
+		$this->prepareForTests();
+	}
+	/**
+     * Migrates the database and set the mailer to 'pretend'.
+     * This will cause the tests to run quickly.
+     *
+     */
+    private function prepareForTests()
+    {
+        Artisan::call('migrate'); //Don't need yet
+        Mail::pretend(true);
+    }
+	// public function testBasicExample()
+	// {
+	// 	$crawler = $this->client->request('GET', '/');
+
+	// 	$this->assertTrue($this->client->getResponse()->isOk());
+	// }
+	public function testIndexPagenoLogin()
+	{
+
+		Auth::shouldReceive('check')->andReturn(false);//Pretends we're not logged in
+
+		$crawler = $this->client->request('GET', '/');
+
+		$this->assertTrue($this->client->getResponse()->isOk());
+	}
+	public function testIndexPageWithLogin()
+	{
+
+		Auth::shouldReceive('check')->andReturn(true); //Pretends we're logged in
+
 		$crawler = $this->client->request('GET', '/');
 
 		$this->assertTrue($this->client->getResponse()->isOk());
