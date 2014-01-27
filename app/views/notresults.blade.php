0fc34851b8cbc98a640102582084ac57119933be
Brian <laxnski@gmail.com>
2014-01-27 09:48:34 -0800

    Added ! to view

--- app/views/notresults.blade.php
+++ app/views/notresults.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.index')
 @section('main')
 <div class="jumbotron">
-	<h1>Results Coming Soon</h1> 
+	<h1>Results Coming Soon!</h1> 
 	<h2> Check Back @ {{{date('g:ia \o\n n/j/y',strtotime($answer_reveal))}}}</h2>
 </div>
 @stop
