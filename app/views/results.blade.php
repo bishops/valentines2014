40f7aa25031874b600d28d39064bbb18ee3d5d74
Brian <laxnski@gmail.com>
2014-01-26 18:08:41 -0800

    Add more views

---
+++ app/views/results.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.index')
+@section('main')
+<div class="jumbotron">
+	<h1>Results</h1>
+</div>
+@stop
---
+++ app/views/thanks.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.index')
+@section('main')
+<div class="jumbotron">
+	<h1>Thanks!</h1>
+	<p>Be sure to come back and check your matches on Valentine's Day</p>
+	<a href="results" class="btn btn-lg btn-primary">Check Results</a>
+</div>
+@stop
